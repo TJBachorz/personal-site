345444ad818f84180ae5aab68eb59a843c55f906
TJBachorz <TJBachorz@gmail.com>
2020-11-20 17:41:44 -0700

    times animations, but fails to fix flashing

---
+++ app.js
@@ -0,0 +1,8 @@
+// document.body.classList.add('js-loading');
+// const img = document.querySelector(".my-pic");
+
+// img.addEventListener("load", removeLoadingClass);
+
+// function removeLoadingClass() {
+//     document.body.classList.remove('js-loading');
+// }
