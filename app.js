a4d6f1fa1fa7490b30ebe5046f2f1ecb1da7bbee
TJBachorz <TJBachorz@gmail.com>
2020-12-01 11:46:25 -0700

    adds modal with crude styling for functionality test

--- app.js
+++ app.js
@@ -1,15 +1,45 @@
-const links = document.querySelectorAll("a")
+console.log("working")
 
-links.forEach(link => {
-    link.addEventListener("click", handleExternalLink)
+
+const modalTriggers = document.querySelectorAll('.fa')
+const modalCloseTrigger = document.querySelector('.popup-modal__close')
+const bodyBlackout = document.querySelector('.body-blackout')
+const popupModal = document.querySelector(".popup-modal")
+
+modalTriggers.forEach(trigger => {
+    trigger.addEventListener('click', () => {
+        const { link } = trigger.dataset
+
+        popupModal.classList.add('is--visible')
+        bodyBlackout.classList.add('is-blacked-out')
+
+        const modalLink = document.querySelector(".modal-link")
+        modalLink.href = link
+    })
+    popupModal.querySelector('.popup-modal__close').addEventListener('click', handleModalClose)
+    
+    bodyBlackout.addEventListener('click', handleModalClose)
 })
 
-function handleExternalLink(event) {
-    const externalLink = event.target.href
-    if (!externalLink.includes("localhost")) {
-        event.preventDefault()
-        if (window.confirm("You are about to leave this site. Are you sure?")) {
-            window.location.href = externalLink
-        }
-    }
+function handleModalClose() {
+    popupModal.classList.remove('is--visible')
+    bodyBlackout.classList.remove('is-blacked-out')
 }
+
+
+
+// const links = document.querySelectorAll("a")
+
+// links.forEach(link => {
+//     link.addEventListener("click", handleExternalLink)
+// })
+
+// function handleExternalLink(event) {
+//     const externalLink = event.target.href
+//     if (!externalLink.includes("localhost")) {
+//         event.preventDefault()
+//         if (window.confirm("You are about to leave this site. Are you sure?")) {
+//             window.location.href = externalLink
+//         }
+//     }
+// }
